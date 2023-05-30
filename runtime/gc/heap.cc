86d57ee1078e67c4f8641dc36830931ecf53fb72
Kunal Sareen <kunal.sareen@anu.edu.au>
2023-05-30 08:45:52 +0000

    Implement Heap::IsValidObjectAddress for MMTk
    Change-Id: If38b02ce78e9cd26dfe15105bd59dc40b6548cc8

--- runtime/gc/heap.cc
+++ runtime/gc/heap.cc
@@ -1894,7 +1894,11 @@ bool Heap::IsValidObjectAddress(const void* addr) const {
   if (addr == nullptr) {
     return true;
   }
+#if ART_USE_MMTK
+  return IsAligned<kObjectAlignment>(addr) && mmtk_is_in_any_space(addr);
+#else
   return IsAligned<kObjectAlignment>(addr) && FindSpaceFromAddress(addr) != nullptr;
+#endif  // ART_USE_MMTK
 }
 
 bool Heap::IsNonDiscontinuousSpaceHeapAddress(const void* addr) const {
