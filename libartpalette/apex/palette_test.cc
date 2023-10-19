8fe0809df69d6bf0524bfb7553592addce2e27ba
Martin Stjernholm <mast@google.com>
2023-10-19 21:26:30 +0100

    Avoid dynamic lib dependencies on internal ART libraries in the libartpalette and dex2oat CTS gtests.
    We cannot use art_standalone_gtest_defaults. dex2oat_cts_test.cc rips
    out the necessary bits from common_art_test.cc instead, with as few
    changes as possible.
    
    With this change we can reenable art_standalone_dex2oat_cts_tests
    again (cf. b/288212464#comment5).
    
    Merged-In set to a CL in main to avoid merging there - gtests are
    already linked statically there.
    
    Cherry picked from
    https://googleplex-android-review.git.corp.google.com/c/platform/art/+/25166821.
    
    Test: m art_standalone_libartpalette_tests \
            art_standalone_dex2oat_cts_tests
      Do `readelf -d` on the test binaries to check that only stable ABI
      libraries are in the NEEDED list.
    Test: cts-tradefed run commandAndExit cts \
            -m art_standalone_dex2oat_cts_tests
      on a tm-release image.
    Test: cts-tradefed run commandAndExit cts \
            -m art_standalone_libartpalette_tests
      on a tm-release image.
    Bug: 306064090
    Bug: 288212464
    Merged-In: I75a844f53663385ef98351f60d3adb900157f5e5
    Merged-In: I4a8d1eddaacf9e476f7d3d783edec0223cb39c04
    Change-Id: I708d83015bc151eba568d5d6f777872274f7580c

--- libartpalette/apex/palette_test.cc
+++ libartpalette/apex/palette_test.cc
@@ -29,6 +29,7 @@
 #endif
 
 #include "gtest/gtest.h"
+#include "nativehelper/JniInvocation.h"                                 \
 
 namespace {
 
@@ -95,6 +96,11 @@ TEST_F(PaletteClientTest, JniInvocation) {
 #else
   bool enabled;
   EXPECT_EQ(PALETTE_STATUS_OK, PaletteShouldReportJniInvocations(&enabled));
+
+  // This calls JniInvocationInit, which is necessary to load the VM. It's not
+  // public but still stable.
+  JniInvocation jni_invocation;
+  ASSERT_TRUE(jni_invocation.Init(nullptr));
 
   JavaVMInitArgs vm_args;
   JavaVMOption options[] = {
