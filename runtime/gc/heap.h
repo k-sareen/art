9cab9c80c3939d38c8fc88f7f2aa5d86d848ad24
Kunal Sareen <kunal.sareen@anu.edu.au>
2023-11-13 02:59:51 +0000

    Properly handle non-moving object allocation
    This commit also fixes the `Heap::IsMovableObject` and
    `Heap::IsObjectLiveLocked` checks. The `Heap::IsMovableObject` check is
    required by the `CopyObjectVisitor` used in `Object::Clone`. Previously,
    since the check always returned `false`, ART used to allocate the new
    objects with the non-moving allocator, quickly filling it up and
    resulting in frequent out-of-memory errors.
    
    Change-Id: Icee3f6983a38864befb383578d1ae22d1f02ad39

--- runtime/gc/heap.h
+++ runtime/gc/heap.h
@@ -1165,7 +1165,8 @@ class Heap {
         collector_type == kCollectorTypeCMC ||
         collector_type == kCollectorTypeCCBackground ||
         collector_type == kCollectorTypeCMCBackground ||
-        collector_type == kCollectorTypeHomogeneousSpaceCompact;
+        collector_type == kCollectorTypeHomogeneousSpaceCompact ||
+        collector_type == kCollectorTypeThirdPartyHeap;
   }
   bool ShouldAllocLargeObject(ObjPtr<mirror::Class> c, size_t byte_count) const
       REQUIRES_SHARED(Locks::mutator_lock_);
