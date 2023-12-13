05fb4d66d085d4a79134a40652c88d9b38730f8b
Jaeheon Yi <jaeheon@google.com>
2023-12-13 14:33:02 -0800

    riscv64: opcodes with simple implementations
    (1) setup
      lunch aosp_riscv64-trunk-userdebug
    
      export ART_TEST_SSH_USER=ubuntu
      export ART_TEST_SSH_HOST=localhost
      export ART_TEST_SSH_PORT=10001
      export ART_TEST_ON_VM=true
    
      . art/tools/buildbot-utils.sh
      art/tools/buildbot-build.sh --target
    
      # Create, boot and configure the VM.
      art/tools/buildbot-vm.sh create
      art/tools/buildbot-vm.sh boot
      art/tools/buildbot-vm.sh setup-ssh  # password: 'ubuntu'
    
      art/tools/buildbot-cleanup-device.sh
      art/tools/buildbot-setup-device.sh
      art/tools/buildbot-sync.sh
    
    (2) test
      art/test.py --target -r --no-prebuild --ndebug --64  -j 12 --cdex-none --interpreter
    
    Test: Run these opcodes against all interpreter
    tests on a Linux RISC-V VM.
    
    Test: Cuttlefish boot
    
    Bug: 283082047
    Change-Id: I979b7e11ad9d7f32fdafa69eda0cc9348f11f381

--- runtime/nterp_helpers.cc
+++ runtime/nterp_helpers.cc
@@ -272,6 +272,13 @@ bool CanMethodUseNterp(ArtMethod* method, InstructionSet isa) {
         case Instruction::CONST_WIDE_32:
         case Instruction::CONST_WIDE:
         case Instruction::CONST_WIDE_HIGH16:
+        case Instruction::MONITOR_ENTER:
+        case Instruction::MONITOR_EXIT:
+        case Instruction::ARRAY_LENGTH:
+        case Instruction::FILLED_NEW_ARRAY:
+        case Instruction::FILLED_NEW_ARRAY_RANGE:
+        case Instruction::FILL_ARRAY_DATA:
+        case Instruction::THROW:
         case Instruction::SPUT:
         case Instruction::SPUT_WIDE:
         case Instruction::SPUT_OBJECT:
