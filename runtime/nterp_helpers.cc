eb4b1f00361aab7135a7b6deaed0d116e3767905
Jaeheon Yi <jaeheon@google.com>
2023-12-12 20:44:44 -0800

    riscv64: SGET variants
    This patch also addresses a flaw in invoke-custom's
    arg setup slow path, revealed by the addition of
    sget-object to Nterp's working set in 952-invoke-custom.
    
    This patch also addresses a flaw in invoke-interface's
    object method handling.
    
    (1) setup
      lunch aosp_riscv64-trunk-userdebug
    
      export ART_TEST_SSH_USER=ubuntu
      export ART_TEST_SSH_HOST=localhost
      export ART_TEST_SSH_PORT=10001
      export ART_TEST_ON_VM=true
    
      . art/tools/buildbot-utils.sh
      art/tools/buildbot-build.sh --target
    
      # Create, boot and configure the VM.
      art/tools/buildbot-vm.sh create
      art/tools/buildbot-vm.sh boot
      art/tools/buildbot-vm.sh setup-ssh  # password: 'ubuntu'
    
      art/tools/buildbot-cleanup-device.sh
      art/tools/buildbot-setup-device.sh
      art/tools/buildbot-sync.sh
    
    (2) test
      art/test.py --target -r --no-prebuild --ndebug --64  -j 12 --cdex-none --interpreter
    
    Test: Run these opcodes against all interpreter
    tests on a Linux RISC-V VM.
    
    Test: Cuttlefish boot
    
    Bug: 283082047
    
    Change-Id: I60caabf910e92f74be9783c08598c36f3a845609

--- runtime/nterp_helpers.cc
+++ runtime/nterp_helpers.cc
@@ -279,6 +279,13 @@ bool CanMethodUseNterp(ArtMethod* method, InstructionSet isa) {
         case Instruction::FILLED_NEW_ARRAY_RANGE:
         case Instruction::FILL_ARRAY_DATA:
         case Instruction::THROW:
+        case Instruction::SGET:
+        case Instruction::SGET_WIDE:
+        case Instruction::SGET_OBJECT:
+        case Instruction::SGET_BOOLEAN:
+        case Instruction::SGET_BYTE:
+        case Instruction::SGET_CHAR:
+        case Instruction::SGET_SHORT:
         case Instruction::SPUT:
         case Instruction::SPUT_WIDE:
         case Instruction::SPUT_OBJECT:
