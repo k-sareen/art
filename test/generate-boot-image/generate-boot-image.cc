a2bba9be1f4bc05793340db9830a6d78d3986e41
Kunal Sareen <kunal.sareen@anu.edu.au>
2025-07-22 04:36:36 +0000

    Force determinism in the boot image generation to hide errors
    This is a hack as forcing determinism hides transient errors we may get
    during the compilation process.
    
    Change-Id: Ia4a32e7721d2df0d5771adf33c4d67a391cd0768

--- test/generate-boot-image/generate-boot-image.cc
+++ test/generate-boot-image/generate-boot-image.cc
@@ -154,6 +154,7 @@ int GenerateBootImage(const Options& options) {
   args.push_back("--image-format=lz4hc");
   args.push_back("--strip");
   args.push_back("--android-root=out/empty");
+  args.push_back("--force-determinism");
 
   std::string path = ART_FORMAT("{}/{}", options.output_dir, options.instruction_set);
   if (!OS::DirectoryExists(path.c_str())) {
